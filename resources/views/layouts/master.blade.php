3f75bd9bcab5f3a5b239a99c79c999c7f0dc36a7
David Baines <dave@lucolo.com>
2016-09-15 16:30:29 -0400

    footer edits

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -107,16 +107,6 @@
                     <span class="copyright">All rights reserved, &copy;2015 - {!! date('Y') !!} Lucolo, Inc</span>
                 </div>
                 <div class="col-md-12">
-                    <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-twitter"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-facebook"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-12">
                     <ul class="list-inline quicklinks">
                         <!-- 
                         <li><a href="#">Privacy Policy</a>
